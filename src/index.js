1ef2775e919da7dd8e39b250cd1511aa75dbf535
elena-louka <louka.elen@gmail.com>
2024-03-22 21:14:21 +0200

    add cors policy and update git ignore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const questionsRoutes = require('./api/routes/questions');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/questions', questionsRoutes);
 
 app.listen(PORT, () => {
